20e6a6142e8643884af89e705816a212a3e3c645
roberto obando <robertoandres24@gmail.com>
2018-04-03 10:20:14 -0300

    refactor in a core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,14 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
-import { UserService } from './user.service';
+import { CoreModule } from './core/core.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent,
     HomeComponent,
     ContactComponent
   ],
@@ -24,10 +20,10 @@ import { UserService } from './user.service';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    CoreModule
   ],
   providers: [
-    UserService
   ],
   bootstrap: [AppComponent]
 })
--- src/app/users/user-single/user-single.component.ts
+++ src/app/users/user-single/user-single.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { UserService } from '../../user.service';
 import { ActivatedRoute } from '@angular/router';
+import { UserService } from '../../core/services/user.service';
 
 @Component({
   selector: 'app-user-single',
