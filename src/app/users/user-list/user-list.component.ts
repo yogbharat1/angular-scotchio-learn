cbf4f3d41265806a7ca569f3f4dbecdaf423f3ab
roberto obando <robertoandres24@gmail.com>
2018-04-02 21:08:51 -0300

    add user service for get users from the github api

--- src/app/users/user-list/user-list.component.ts
+++ src/app/users/user-list/user-list.component.ts
@@ -1,19 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../user.service';
 
 @Component({
   selector: 'app-user-list',
   template: `
-    <p>
-      user-list works!
-    </p>
+  <section class="section">
+    <div class="container">
+      <div class="columns is-multiline" *ngIf="users">
+      <div class="column is-4" *ngFor="let user of users | async">
+        <div class="card">
+          <div class="card-content">
+            <a routerLink= "/users/{{ user.login }}">   
+              {{ user.login }}
+            </a>
+          </div>
+        </div>  
+      </div>
+    </div>
+    </div>
+  </section>
   `,
   styles: []
 })
 export class UserListComponent implements OnInit {
-
-  constructor() { }
+  users;
+  constructor(private userService: UserService) { }
 
   ngOnInit() {
+    this.users = this.userService.getUsers();
   }
 
 }
--- src/app/users/user-single/user-single.component.ts
+++ src/app/users/user-single/user-single.component.ts
@@ -1,19 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../user.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user-single',
   template: `
-    <p>
-      user-single works!
-    </p>
+    <section class="section">
+      <div class="container">
+        <div class="card" *ngIf="user">
+          <img [src]="user.avatar_url" alt="">
+          <h2> {{ user.login }}</h2>
+        </div>
+      </div>
+    </section>
   `,
   styles: []
 })
 export class UserSingleComponent implements OnInit {
-
-  constructor() { }
+  user;
+  constructor(
+    private userService: UserService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    this.route.params.subscribe(params => {
+      const username = params['username'];
+      this.userService.getUser(username)
+        .subscribe(user => {
+          this.user = user;
+        });
+    })
   }
 
 }
